2f759c028fe7734e5f5a1abf60d8267a0891813f
Artem Kalinin <akalinin.v@gmail.com>
2015-01-26 20:31:15 +0300

    New fiels

--- wp-content/themes/twentyeleven/footer.php
+++ wp-content/themes/twentyeleven/footer.php
@@ -14,6 +14,42 @@
 
 	<footer id="colophon" role="contentinfo">
 
+<?php
+if ( isset($_POST["email-name"]) and isset($_POST["email-phone"]) and isset($_POST["email-company"])) {
+		$name = $_POST['email-name'];
+		$phone = $_POST['email-phone'];
+		$company = $_POST['email-company'];
+          require 'vendor/autoload.php';
+          $sendgrid = new SendGrid(getenv('SENDGRID_USERNAME'), getenv('SENDGRID_PASSWORD'));
+          $message = new SendGrid\Email();
+          $message->addTo(getenv('SENDGRID_EMAIL'))->
+            addTo("akalinin.cr@gmail.com")->
+            setFrom(getenv('SENDGRID_EMAIL'))->
+            setSubject('d8ii')->
+            setText($_POST["email-name"])->
+            setHtml("<strong>{$_POST['email-phone']}. {$_POST['email-company']}</strong>");
+            $response = $sendgrid->send($message);
+          echo "Message sent success ".$response;
+		// echo "{$_POST['email-company']}.{$_POST['email-phone']}.{$_POST['email-name']}";
+		// echo "".$_POST['email-company'];
+      } else {
+          echo "Message sent error";
+      }
+?>
+
+            <form action=""  id="contactForm" method="post">
+            <div class="col-sm-4">
+              <input style="width: 100%;" type="text" name="email-name" value="" size="40" class="d8ii-footer-input wpcf7-form-control wpcf7-text wpcf7-validates-as-required" aria-required="true" aria-invalid="false" placeholder="Name">
+              <input style="width: 100%;" type="text" name="email-phone" value="" size="40" class="d8ii-footer-input wpcf7-form-control wpcf7-text wpcf7-validates-as-required" aria-required="true" aria-invalid="false" placeholder="Company">
+            </div>
+            <div class="col-sm-4">
+              <input style="width: 100%;" type="text" name="email-company" value="" size="40" class="d8ii-footer-input wpcf7-form-control wpcf7-text wpcf7-validates-as-required" aria-required="true" aria-invalid="false" placeholder="Phone">
+              <a  type="submit"  style="width: 100%;" class="sf-button standard grey sf-icon-reveal ml0" href="javascript:{}" onclick="document.getElementById('contactForm').submit();" target="_self"><i class="ss-chat"></i><span class="text">SEND</span></a>
+            </div>
+            </form>
+
+
+
 			<?php
 				/*
 				 * A sidebar in the footer? Yep. You can can customize
